92374cb6eed3b5ec5c233a33d6bad7a4f2d2b231
Filip Sreckovic <filipsreckovic@gmail.com>
2026-02-20 22:35:23 +0100

    add LazyPropagation.cpp range_query

--- NapredneStrukture/EfikasnoPovecavanjeRaspona/LazyPropagation.cpp
+++ NapredneStrukture/EfikasnoPovecavanjeRaspona/LazyPropagation.cpp
@@ -15,7 +15,7 @@ struct SegTree
 
         Point Update (logn)
 
-        Range Update (logn)
+        Range Update (logn) done
 
         Range Set (log n)
 
@@ -98,10 +98,17 @@ struct SegTree
         updates the current node, and pushes the required updates to lower nodes
     */
 
-    void propagate(int nodeIndex, int range)
+    void propagateAndUpdate(int nodeIndex, int range)
     {
-        if(tree[nodeIndex].updated() || nodeIndex * 2 >= this->size) return;
-        
+        //nothing to do
+        if(tree[nodeIndex].updated()) return;
+        //nowhere to propagate (leaf node)
+        if(nodeIndex * 2 >= this->size)
+        {
+            tree[nodeIndex].update(range);
+            return;
+        }
+
         int lchild = nodeIndex * 2, rchild = nodeIndex * 2 + 1;
         
         //push the updates down
@@ -112,27 +119,56 @@ struct SegTree
         tree[nodeIndex].update(range);
     }
 
-    void range_increase_update(int currNodeIndex, int currLeft, int currRight, int segLeft, int segRight, int value)
+    void range_increase_update(int currIndex, int currLeft, int currRight, int segLeft, int segRight, int value)
     {
-        propagate(currNodeIndex, currRight - currLeft + 1);
+        //handle leftover propagations
+        propagateAndUpdate(currIndex, currRight - currLeft + 1);
 
+        //if disjoint, nothing to update
         if(currLeft > segRight || currRight < segLeft) return;
+        //if fully included, update 
         else if(currLeft >= segLeft && currRight <= segRight)
         {
-            tree[currNodeIndex].queue_update(value, 0, false);
-            propagate(currNodeIndex, currRight - currLeft + 1);
+            tree[currIndex].queue_update(value, 0, false);
+            propagateAndUpdate(currIndex, currRight - currLeft + 1);
         }
+        //if semi included update parts where it is included, and update the end result
         else
         {
             int s = currLeft + (currRight - currLeft) / 2;
 
-            range_increase_update(currNodeIndex * 2, currLeft, s, segLeft, segRight, value);
-            range_increase_update(currNodeIndex * 2 + 1, s + 1, currRight, segLeft, segRight, value);
+            range_increase_update(currIndex * 2, currLeft, s, segLeft, segRight, value);
+            range_increase_update(currIndex * 2 + 1, s + 1, currRight, segLeft, segRight, value);
 
-            tree[currNodeIndex].value = tree[currNodeIndex * 2].value + tree[currNodeIndex * 2 + 1].value;
+            tree[currIndex].value = tree[currIndex * 2].value + tree[currIndex * 2 + 1].value;
         }   
     }
 
+    int range_query(int currIndex, int currLeft, int currRight, int segLeft, int segRight)
+    {
+        //handle leftover propagations
+        propagateAndUpdate(currIndex, currRight - currLeft + 1);
+
+        //if disjoint, nothing to give
+        if(currLeft > segRight || currRight < segLeft)
+        {
+            return 0;
+        }
+        //if fully complete, give everything
+        else if(currLeft >= segLeft && currRight <= segRight)
+        {
+            return tree[currIndex].value;
+        }
+        //if partially give sum of left and right
+        else
+        {
+            int s = currLeft + (currRight - currLeft) / 2;
+
+            return  range_query(currIndex * 2,     currLeft, s        , segLeft, segRight) + 
+                    range_query(currIndex * 2 + 1, s + 1   , currRight, segLeft, segRight);
+        }
+
+    }
 
     public:
 
@@ -178,12 +214,18 @@ struct SegTree
 
     }
 
+    //inclusive, 0 indexed
     void range_increase_update(int l, int r, int value)
     {
         range_increase_update(1, 0, this->size / 2 - 1, l, r, value);
     }
-    
 
+
+    //inclusive, 0 indexed
+    int range_query(int l, int r)
+    {
+        return range_query(1, 0, this->size / 2 - 1, l, r);
+    }
 
 };
 
@@ -198,6 +240,10 @@ int main()
 
     st.range_increase_update(0, 3, 100);
 
+    std::cout << "query one: " << st.range_query(0, 0);
+    std::cout << "\nquery two: " << st.range_query(0, 1);
+    std::cout << "\nquery three: " << st.range_query(0, n - 1);
+    std::cout << std::endl;
     st.printTree();
 
     return 0;
