878f54235bb1edf9d738cc36a51b7e66d65adeb1
Filip Sreckovic <filipsreckovic@gmail.com>
2026-02-20 22:17:07 +0100

    range_increase_update method along with various help methods

--- NapredneStrukture/EfikasnoPovecavanjeRaspona/LazyPropagation.cpp
+++ NapredneStrukture/EfikasnoPovecavanjeRaspona/LazyPropagation.cpp
@@ -43,10 +43,43 @@ struct SegTree
             this->shouldSet = false;
         }
 
+        void queue_update(int adjust, int set, int shouldSet)
+        {
+            if(shouldSet)
+            {
+                this->adjust = 0;
+                this->set = set;
+            }
+            else if(this->shouldSet)
+            {
+                this->set += adjust;
+            }
+            else
+            {
+                this->adjust += adjust;   
+            }
+        }
+
         bool updated()
         {
-            return set + adjust;
+            return !(adjust + shouldSet);
         }
+
+        void update(int range)
+        {
+            value = set * range * shouldSet + (value +  range * adjust) * !shouldSet;
+
+            SegNode propagateNode = SegNode();
+
+            propagateNode.adjust = this->adjust;
+            propagateNode.set = this->set;
+            propagateNode.shouldSet = this->set;
+
+            this->adjust = 0;
+            this->set = 0;
+            this->shouldSet = false;
+        }
+
 
     };
 
@@ -61,6 +94,45 @@ struct SegTree
         return k;
     }
 
+    /*
+        updates the current node, and pushes the required updates to lower nodes
+    */
+
+    void propagate(int nodeIndex, int range)
+    {
+        if(tree[nodeIndex].updated() || nodeIndex * 2 >= this->size) return;
+        
+        int lchild = nodeIndex * 2, rchild = nodeIndex * 2 + 1;
+        
+        //push the updates down
+        tree[lchild].queue_update(tree[nodeIndex].adjust, tree[nodeIndex].set, tree[nodeIndex].shouldSet);
+        tree[rchild].queue_update(tree[nodeIndex].adjust, tree[nodeIndex].set, tree[nodeIndex].shouldSet);
+
+        //update currentNode and clear it's queue
+        tree[nodeIndex].update(range);
+    }
+
+    void range_increase_update(int currNodeIndex, int currLeft, int currRight, int segLeft, int segRight, int value)
+    {
+        propagate(currNodeIndex, currRight - currLeft + 1);
+
+        if(currLeft > segRight || currRight < segLeft) return;
+        else if(currLeft >= segLeft && currRight <= segRight)
+        {
+            tree[currNodeIndex].queue_update(value, 0, false);
+            propagate(currNodeIndex, currRight - currLeft + 1);
+        }
+        else
+        {
+            int s = currLeft + (currRight - currLeft) / 2;
+
+            range_increase_update(currNodeIndex * 2, currLeft, s, segLeft, segRight, value);
+            range_increase_update(currNodeIndex * 2 + 1, s + 1, currRight, segLeft, segRight, value);
+
+            tree[currNodeIndex].value = tree[currNodeIndex * 2].value + tree[currNodeIndex * 2 + 1].value;
+        }   
+    }
+
 
     public:
 
@@ -72,7 +144,7 @@ struct SegTree
         this->size = closestPow2(ogArr.size() * 2);
         tree.resize(this->size, SegNode());
 
-        for(int i = this->size / 2; i < this->size; i++)
+        for(int i = this->size / 2; i - this->size / 2 < ogArr.size(); i++)
         {
             tree[i].value = ogArr[i - this->size / 2];
         }
@@ -89,12 +161,44 @@ struct SegTree
         tree.resize(this->size, SegNode());
     }
 
+    void printTree()
+    {
+        std::cout << "Main Tree: \n" ;
+        for(int i = 1; i < this->size; i++)
+        {
+            std::cout << i << ": " << tree[i].value << " | ";
+            if(i + 1 == closestPow2(i + 1)) std::cout << std::endl;
+        }
+        std::cout << "Lazy Tree \n";
+        for(int i = 1; i < this->size; i++)
+        {
+            std::cout << i << ": " << tree[i].adjust << " | ";
+            if(i + 1 == closestPow2(i + 1)) std::cout << std::endl;
+        }
+
+    }
+
+    void range_increase_update(int l, int r, int value)
+    {
+        range_increase_update(1, 0, this->size / 2 - 1, l, r, value);
+    }
     
+
+
 };
 
 
 
 int main()
 {
+    int n = 12;
+    std::vector<int> arr = {5, -2, 13, 0, 7, 7, -9, 4, 18, -1, 6, 2};
+
+    SegTree st = SegTree(arr);
+
+    st.range_increase_update(0, 3, 100);
+
+    st.printTree();
+
     return 0;
 }
